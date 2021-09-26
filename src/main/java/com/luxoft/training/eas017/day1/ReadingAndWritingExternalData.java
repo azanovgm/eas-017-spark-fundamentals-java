c5e9e3afccf1c8c6a7732b1ebc8a32d8923d4fea
dextravaganz <denis.m.smirnov@gmail.com>
2021-09-26 22:03:17 +0300

    Remove demo and solution code

--- src/main/java/com/luxoft/training/eas017/day1/ReadingAndWritingExternalData.java
+++ src/main/java/com/luxoft/training/eas017/day1/ReadingAndWritingExternalData.java
@@ -5,8 +5,6 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
-import java.util.Arrays;
-
 public class ReadingAndWritingExternalData {
 
     public static void main(String[] args)  {
@@ -14,35 +12,12 @@ public class ReadingAndWritingExternalData {
         final SparkConf conf = new SparkConf().setAppName("AggregateIllustrated").setMaster("local[*]");
         final JavaSparkContext sc = new JavaSparkContext(conf);
 
-        // We can read a single text file
-        JavaRDD<String> textFile = sc.textFile("src/main/resources/day1/file01.csv");
-
-        // We can read multiple files
-        textFile = sc.textFile("src/main/resources/day1/file01.csv,src/main/resources/day1/file02.csv");
-
-        // We can read a file using wildcard
-        textFile = sc.textFile("src/main/resources/day1/file*.csv");
+        JavaRDD<String> textFile = null;
 
         textFile.foreach(s -> System.out.println(s));
 
         //TODO: using a single RDD with all rows calculate the number of individual row elements ignoring header
-        String header = textFile.first();
-        long numberOfRowElements = textFile
-            .filter(s -> !s.equals(header))
-            .flatMap(s -> Arrays.stream(s.split(",")).iterator())
-            .count();
-        System.out.println("Number of all row elements: " + numberOfRowElements);
 
-        JavaRDD<String> jsons = textFile
-            .filter(s -> !s.equals(header))
-            .map(s ->{
-                String[] values = s.split(",");
-                return String.format("{\"a\": %s,\"b\": %s,\"c\": %s}",
-                        values[0], values[1], values[2]);
-            });
-        jsons.saveAsTextFile("src/main/resources/day1/out.json");
-
-        System.out.println(textFile.getNumPartitions());
     }
 
 }
